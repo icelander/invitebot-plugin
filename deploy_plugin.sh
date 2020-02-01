099902711dec22c0ad80ae6f341335dba7f7a4fa
Paul Rothrock <paul@movetoiceland.com>
2020-02-01 09:20:16 -0500

    Updated CHANGELOG
    Signed-off-by: Paul Rothrock <paul@movetoiceland.com>

--- deploy_plugin.sh
+++ deploy_plugin.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-# TODO: Build tar file name from latest tag
-archive_name="dist/invitebot-0.1.0.tar.gz"
+jq_cmd=jq
+[[ $(type -P "$jq_cmd") ]] || { 
+	echo "'$jq_cmd' command line JSON processor not found";
+	echo "Please install on linux with 'sudo apt-get install jq'"
+	echo "Please install on mac with 'brew install jq'"
+	exit 1; 
+}
+
+mmctl_cmd=mmctl
+[[ $(type -P "$mmctl_cmd") ]] || { 
+	echo "'$mmctl_cmd' command line JSON processor not found";
+	echo "Please install on linux with 'sudo apt-get install mmctl'"
+	echo "Please install on mac with 'brew install mmctl'"
+	exit 1; 
+}
+
+
+id=`${jq_cmd} -r '.id' plugin.json`
+name=`${jq_cmd} -r '.name' plugin.json`
+version=`${jq_cmd} -r '.version' plugin.json`
+archive_name="dist/${id}-${version}.tar.gz"
+
+${mmctl_cmd} auth current
+
+read -p "To deploy ${name} to the above server, press enter:"
 
 # mmctl makes this *easy*
-mmctl plugin disable invitebot
-mmctl plugin delete invitebot
-mmctl plugin add $archive_name
-mmctl plugin enable invitebot
+${mmctl_cmd} plugin disable $id
+${mmctl_cmd} plugin delete $id
+${mmctl_cmd} plugin add $archive_name
+${mmctl_cmd} plugin enable $id
